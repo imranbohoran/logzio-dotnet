f94931a3de1304d7e13ce4034141aa1f76a5304e
Admon Sasson <admon.sasson@snyk.io>
2021-11-02 11:39:14 +0200

    tests

---
+++ src/test234/LogzioLog4netSampleApplication/Program.cs
@@ -0,0 +1,16 @@
+﻿using log4net;
+using log4net.Config;
+
+namespace LogzioLog4netSampleApplication
+{
+	public class Program
+	{
+		static void Main()
+		{
+			XmlConfigurator.Configure();
+
+			var logger = LogManager.GetLogger("GreetingsLogger");
+			logger.Info("Greetings, earthling.");
+		}
+	}
+}
